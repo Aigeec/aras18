ef6f2d942b0c8ffa4f261429bbdc5e932dd3e19a
Aodhagan Collins <aodhaganc@gmail.com>
2018-09-04 21:33:13 +0100

    fix(server): client side routing failed when route was use to load page
    - added history api fallback

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const compression = require('compression')
+const fallback = require('express-history-api-fallback')
+
+const publicPath = path.join(__dirname, 'public')
 
 app.use(compression())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(publicPath))
+app.use(fallback('index.html', { publicPath }))
 
 module.exports = app
