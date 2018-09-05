555f5b9a95d8c5794784b0601a5ebef82046b5de
Aodhagán Collins <aodhaganc@gmail.com>
2018-09-05 21:58:24 +0100

    feat(server): add helmet protections

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const helmet = require('helmet')
 const compression = require('compression')
 const fallback = require('express-history-api-fallback')
 
 const publicPath = path.join(__dirname, 'public')
 
+app.use(helmet())
 app.use(compression())
+
 app.use(express.static(publicPath), { maxAge: '30 days' })
-app.use(fallback('index.html', { publicPath }))
+app.use(fallback('index.html', { root: publicPath }))
 
 module.exports = app
